a6ea8bc6ed023639e7426339549e5773f5501c6f
Avdigital <aerovitiondigital@gmail.com>
2016-10-04 16:32:43 -0700

    Update avd_angularjsWebApp.js

--- avd_angularjsWebApp.js
+++ avd_angularjsWebApp.js
@@ -22,10 +22,6 @@
 __________________________________________________________________________________________________________________________________________
 */
  
-
- 
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <!-- AppName User SearchAdd -->
 <html ng-app="userApp">
